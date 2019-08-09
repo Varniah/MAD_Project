eb95c15ece5ba751c69a8077fae1bf75bfcdad3a
varniah <varniah26@gmail.com>
2019-08-09 20:01:08 +0530

    success

---
+++ app/src/main/java/com/example/explore/RestaurantsinDetailed.java
@@ -0,0 +1,14 @@
+package com.example.explore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantsinDetailed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurantsin_detailed);
+    }
+}
